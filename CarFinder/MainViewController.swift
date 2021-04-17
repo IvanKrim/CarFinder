7861d856142e61b75f49c0e5c70113ee94857e07
Artem Sokolovskiy <artemsokol98@gmail.com>
2021-04-17 23:33:13 +0300

    add comment

--- CarFinder/MainViewController.swift
+++ CarFinder/MainViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.9538531899, green: 0.9695010781, blue: 0.9060884118, alpha: 1)
     }
-
+    //comment
 
 }
 
