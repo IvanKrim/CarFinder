b1b98dffb7bfecd34c7c43378f1288cca101ecf3
JohnKrim <reamonn96@gmail.com>
2021-04-17 23:52:16 +0300

    Data manager added

---
+++ CarFinder/ServiceDataManager.swift
@@ -0,0 +1,24 @@
+//
+//  ServiceDataManager.swift
+//  CarFinder
+//
+//  Created by Kalabishka Ivan on 17.04.2021.
+//
+
+class DataManager {
+    let carModel = [
+    "Audi A4", "BMW 3", "Mercedes-Benz E-class", "Mazda 6", "Ford Mustang"
+    ]
+    
+    let yearOfCarManufacture = [
+        "2020", "2020", "2019", "2020", "2020"
+    ]
+    
+    let carColor = [
+    "Синий", "Белый", "Белый", "Красный", "Синий"
+    ]
+    
+    let carEngine = [
+    "Бензин", "Дизель"
+    ]
+}
