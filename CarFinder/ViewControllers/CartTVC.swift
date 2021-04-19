f7ea653b14e182e7e4ed28770d3145f0bdd2fb14
JohnKrim <reamonn96@gmail.com>
2021-04-19 23:51:54 +0300

    cartVC added

---
+++ CarFinder/ViewControllers/CartTVC.swift
@@ -0,0 +1,39 @@
+//
+//  CartTVC.swift
+//  CarFinder
+//
+//  Created by Kalabishka Ivan on 19.04.2021.
+//
+
+import UIKit
+
+class CartTVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
