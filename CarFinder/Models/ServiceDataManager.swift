8eefd44aebea90c4cf0f9fd781a9be0481cc92a7
Yura Chekalyuk <yura_chekalyuk@mail.ru>
2021-04-19 21:10:28 +0300

    +add new car(Tesla Mosel S)

--- CarFinder/Models/ServiceDataManager.swift
+++ CarFinder/Models/ServiceDataManager.swift
@@ -14,26 +14,30 @@ class DataManager {
     
     let carModels = [
         "Audi A4", "BMW 3", "Mercedes-Benz E-class",
-        "Mazda 6", "Mazda CX-9", "Ford Mustang"
+        "Mazda 6", "Mazda CX-9", "Ford Mustang",
+        "Tesla Model S"
     ]
     
     let carPrices = [
         "1 500 000 руб.", "1 650 000 руб.", "1 850 000 руб.",
-        "2 000 000 руб.", "3 050 000 руб.", "2 100 000 руб."
+        "2 000 000 руб.", "3 050 000 руб.", "2 100 000 руб.",
+        "7 900 000 руб"
     ]
     
     let yearsOfCarManufacture = [
         "2020", "2020", "2019",
-        "2020","2021", "2020"
+        "2020","2021", "2020",
+        "2021"
     ]
     
     let carColors = [
         "Синий", "Белый", "Белый",
-        "Красный", "Белый", "Синий"
+        "Красный", "Белый", "Синий",
+        "Красный"
     ]
     
     let carEngines = [
-        "Бензин", "Дизель"
+        "Бензин", "Дизель", "Электро"
     ]
     
     private init() {}
